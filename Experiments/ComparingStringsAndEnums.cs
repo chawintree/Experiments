ef1849cfdcbb96950ecd24c214f7f6149cc7c28a
Chawin Tree <hello@chawintree.com>
2020-02-16 19:42:05 +0000

    Initial commit

---
+++ Experiments/ComparingStringsAndEnums.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Experiments
+{
+    public class ComparingStringsAndEnums
+    {
+        public enum TestEnum { Foo }
+        
+        [Params("Foo", "foo")]
+        public string MyString { get; set; }
+        
+        [Params(TestEnum.Foo)]
+        public TestEnum MyEnum { get; set; }
+
+        [Benchmark]
+        public void ToStringComparison() => MyEnum.ToString().Equals(MyString, StringComparison.OrdinalIgnoreCase);
+
+        [Benchmark]
+        public void CompareComparison() => string.Compare(MyEnum.ToString(), MyString, StringComparison.OrdinalIgnoreCase);
+        
+        [Benchmark]
+        public void LowercaseComparison() => MyEnum.ToString().ToLower().Equals(MyString.ToLower());
+
+        [Benchmark]
+        public void CastComparison() => MyEnum.Equals(Enum.Parse(typeof(TestEnum), MyString, true));
+    }
+}
---
+++ Experiments/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Experiments.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ComparingStringsAndEnums>();
+        }
+    }
+}
